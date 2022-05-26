df7372d0e5889d59c39ed1bd3ec56c46a1b0f57f
Andrew Jazbec <hello@ajzbc.com>
2022-05-25 17:27:28 -0700

    footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,28 @@
 <script>
 	import '../app.css';
+
+	import Icon from '../lib/components/Icon.svelte';
 </script>
 
 <slot />
+
+<footer class="mt-auto flex items-center justify-center gap-4 p-4 pb-8 text-xl">
+	<a href="//ajzbc.com" target="_blank">
+		<Icon icon="at" />
+		<span>ajzbc</span>
+	</a>
+	<a href="//github.com/ajzbc/spotify-stats" target="_blank">
+		<Icon icon="github" />
+		<span>source</span>
+	</a>
+</footer>
+
+<style lang="postcss">
+	a {
+		@apply flex items-center justify-center gap-1 text-spotify-green;
+	}
+
+	a span {
+		@apply text-white;
+	}
+</style>
