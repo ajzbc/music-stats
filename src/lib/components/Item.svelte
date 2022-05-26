1b7b04aa7a6ad52262296d88aa1e6f36ba00e7af
Andrew Jazbec <hello@ajzbc.com>
2022-05-25 17:27:20 -0700

    fade in

--- src/lib/components/Item.svelte
+++ src/lib/components/Item.svelte
@@ -1,25 +1,30 @@
 <script>
+	import { fade } from 'svelte/transition';
+
 	export let item = {};
-	export let type;
 	export let i = 1;
+	export let type;
 </script>
 
-<div class="flex gap-4 group {type == 'tracks' ? 'sm:items-center' : 'items-center'} relative">
+<div
+	class="group flex gap-4 {type == 'tracks' ? 'sm:items-center' : 'items-center'} relative"
+	in:fade={{ duration: 100 }}
+>
 	<span
-		class="absolute left-0 translate-x-[-3rem] text-2xl font-bold font-mono select-none text-right w-8"
+		class="font-mono absolute left-0 w-8 translate-x-[-3rem] select-none text-right text-2xl font-bold"
 		>{i + 1}</span
 	>
 	<a href={type == 'tracks' ? item?.album?.link : item.link} target="_blank">
 		<img
 			src={item.art}
 			alt="{item.name} cover"
-			class="h-24 w-24 sm:h-32 sm:w-32 group-hover:shadow-lg group-hover:shadow-spotify-green
+			class="h-24 w-24 transition ease-in-out group-hover:shadow-lg group-hover:shadow-spotify-green sm:h-32 sm:w-32
             {type == 'tracks' ? 'rounded-lg' : 'rounded-full'} "
 		/>
 	</a>
 	<div class="flex-1">
 		{#if type == 'tracks'}
-			<a href={item.link} target="_blank" class="text-xl sm:text-2xl block mb-1 font-medium"
+			<a href={item.link} target="_blank" class="mb-1 block text-xl font-medium sm:text-2xl"
 				>{item.name}</a
 			>
 			{#if item.artists}
@@ -28,12 +33,12 @@
 					<a
 						href={artist.link}
 						target="_blank"
-						class="text-lg sm:text-xl text-spotify-green hover:underline">{artist.name}</a
+						class="text-lg text-spotify-green hover:underline sm:text-xl">{artist.name}</a
 					>
 				{/each}
 			{/if}
 		{:else if type == 'artists'}
-			<a href={item.link} target="_blank" class="text-xl sm:text-2xl block mb-1 font-medium"
+			<a href={item.link} target="_blank" class="mb-1 block text-xl font-medium sm:text-2xl"
 				>{item.name}</a
 			>
 		{/if}
