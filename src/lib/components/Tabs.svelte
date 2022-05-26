e38b5b52e5d3a26fd360f51ea7c58e1b9c068bdf
Andrew Jazbec <hello@ajzbc.com>
2022-05-25 17:25:19 -0700

    tailwind class reordering

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -13,14 +13,14 @@
 </script>
 
 <ul
-	class="p-1 border-2 border-spotify-green border-opacity-30 rounded-lg mr-auto sm:mr-0 first:mb-2 sm:first:mb-0"
+	class="mr-auto rounded-lg border-2 border-spotify-green border-opacity-30 p-1 first:mb-2 sm:mr-0 sm:first:mb-0"
 >
 	{#each tabs as tab}
 		<li class="inline-flex">
 			<button
 				class="
                     {active == tab.value ? 'active' : ''}
-                    px-2 py-1 rounded-md font-semibold text-lg text-spotify-green
+                    rounded-md px-2 py-1 text-lg font-semibold text-spotify-green
                 "
 				on:click={() => (active = tab.value)}
 			>
