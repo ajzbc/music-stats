6b95cc2620cc636878ed3459ed3beeab297b66df
Andrew Jazbec <hello@ajzbc.com>
2022-05-25 17:25:39 -0700

    new font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			sans: ['SFRounded', 'sans-serif']
+		},
 		extend: {
 			colors: {
 				'spotify-green': '#1DB954'
