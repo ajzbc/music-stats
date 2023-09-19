71f6c689fd7e361c0b1d2d97b3e988201ecbbaa1
Andrew Jazbec <hello@ajzbc.com>
2023-09-18 22:33:50 -0700

    spotify icon

--- src/lib/components/Item.svelte
+++ src/lib/components/Item.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { fade } from 'svelte/transition';
+	import Icon from './Icon.svelte';
 
 	export let item = {};
 	export let i = 1;
@@ -14,13 +15,21 @@
 		class="font-mono absolute left-0 w-8 translate-x-[-3rem] select-none text-right text-2xl font-bold"
 		>{i + 1}</span
 	>
-	<a href={type == 'tracks' ? item?.album?.link : item.link} target="_blank" rel="noreferrer">
+	<a
+		href={type == 'tracks' ? item?.album?.link : item.link}
+		target="_blank"
+		rel="noreferrer"
+		class="relative"
+	>
 		<img
 			src={item.art}
 			alt="{item.name} cover"
 			class="h-24 w-24 transition ease-in-out group-hover:shadow-lg group-hover:shadow-spotify-green sm:h-32 sm:w-32
-            {type == 'tracks' ? 'rounded-lg' : 'rounded-full'} "
+            {type == 'tracks' ? '' : 'rounded-full'} "
 		/>
+		<div class="absolute bottom-0 right-0 rounded-tl-lg bg-black p-0.5">
+			<Icon icon="spotify" classes="h-6 w-6 text-spotify-green" />
+		</div>
 	</a>
 	<div class="flex-1">
 		{#if type == 'tracks'}
